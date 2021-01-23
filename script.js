5b0c7daea5f865ebf842118b870888f184c24d7b
Sophie P <sonya.pav25@gmail.com>
2021-01-23 17:53:32 +0300

    Add figures, falling animation

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ for (let y = 18; y > 0; y--) {
 
 // initial coordinates of first (lower left) square of the figure
 // next coordinates are counted in left to right, low to up direction
-let x = 5, y = 10;
+let x = 5, y = 15;
 let mainArr = [
     //line
     [
@@ -40,18 +40,61 @@ let mainArr = [
         [1, 1]
     ],
 
+    //L-shape
+    [
+        [1, 0],
+        [0, 1],
+        [0, 2]
+    ],
+
+    //mirrored L-shape
+    [
+        [1, 0],
+        [1, 1],
+        [1, 2]
+    ],
+
+    //Z-shape
+    [
+        [1, 0],
+        [-1, 1],
+        [0, 1]
+    ],
+
+    //mirrored Z-shape
+    [
+        [1, 0],
+        [1, 1],
+        [2, 1]
+    ],
+
+    //T-shape
+    [
+        [0, 1],
+        [1, 1],
+        [-1, 1]
+    ],
+
+    //T-shape (upside down)
+    [
+        [1, 0],
+        [2, 0],
+        [1, 1]
+    ]
 ]
 
 let currentFigure = 0;
 let figureBody = 0;
 
 function create() {
+    //choose random figure from array
     function getRandom() {
         return Math.round(Math.random() * (mainArr.length - 1))
     }
 
     currentFigure = getRandom();
 
+    //coordinates of each cell of a figure
     figureBody = [
         document.querySelector(`[posX = "${x}"][posY = "${y}"]`),
         document.querySelector(`[posX = "${x + mainArr[currentFigure][0][0]}"][posY = "${y + mainArr[currentFigure][0][1]}"]`),
@@ -65,3 +108,50 @@ function create() {
 }
 
 create();
+
+function move() {
+    let moveFlag = true;
+    let coordinates = [
+        [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')],
+        [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')],
+        [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')],
+        [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')]
+    ];
+
+    for (let i = 0; i < coordinates.length; i++) {
+        //if last row or something below
+        if (coordinates[i][1] == 1 || document.querySelector(`[posX = "${coordinates[i][0]}"][posY = "${coordinates[i][1] - 1}"]`).classList.contains('set')) {
+            moveFlag = false;
+            break;
+        }
+    }
+
+    if (moveFlag) {
+        //remove class to decolor cells
+        for (let i = 0; i < figureBody.length; i++) {
+            figureBody[i].classList.remove('figure');
+        }
+
+        figureBody = [
+            document.querySelector(`[posX = "${coordinates[0][0]}"][posY = "${coordinates[0][1] - 1}"]`),
+            document.querySelector(`[posX = "${coordinates[1][0]}"][posY = "${coordinates[1][1] - 1}"]`),
+            document.querySelector(`[posX = "${coordinates[2][0]}"][posY = "${coordinates[2][1] - 1}"]`),
+            document.querySelector(`[posX = "${coordinates[3][0]}"][posY = "${coordinates[3][1] - 1}"]`)
+        ]
+
+        //color cells of the figure again
+        for (let i = 0; i < figureBody.length; i++) {
+            figureBody[i].classList.add('figure');
+        }
+    } else {
+        for (let i = 0; i < figureBody.length; i++) {
+            figureBody[i].classList.remove('figure');
+            figureBody[i].classList.add('set');
+        }
+        create();
+    }
+}
+
+let interval = setInterval(() => {
+    move();
+}, 300)
